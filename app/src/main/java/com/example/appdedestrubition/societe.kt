1c34fc22e5e5f19708f8b39a11f577c15f48cf85
ibraben87 <ibenmansour87@gmail.com>
2022-04-30 03:45:06 +0100

    MY WORK

---
+++ app/src/main/java/com/example/appdedestrubition/societe.kt
@@ -0,0 +1,13 @@
+package com.example.appdedestrubition
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class societe : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_societe)
+        val consulter_commande:Button
+    }
+}
